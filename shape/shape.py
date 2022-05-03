d7a7b10ed68a9995df3433a1cb4bd05d23a8f2c8
gokcelbilgin <gokcelbilgin@gmail.com>
2022-05-03 15:28:51 +0300

    Implement 90 degree rotation

--- shape/shape.py
+++ shape/shape.py
@@ -14,18 +14,47 @@ class Shape(ABC):
     def __init__(self, starting_coordinates, square_size):
         self.sc = starting_coordinates
         self.sqsz = square_size
-        self.color = self.random_color()
+        self.color = self.__random_color()
 
-    def random_color(self):
+    def __random_color(self):
         return Shape.COLORS[random.randint(0, len(Shape.COLORS) - 1)]
 
-    @abstractmethod
     def coordinates(self):
-        raise NotImplementedError
+        print('coordinates method running')
+        rotations = [0, 90, 180, 270]
+        rotation_degree = rotations[random.randint(0, len(rotations) - 1)]
+        if rotation_degree == 0:
+            print('rotation degree is 0')
+            return self.init_coordinates()
 
-    # @abstractmethod
-    # def rotatations(self):
-    #     raise NotImplementedError
+        return self.rotate(90)
+
+    def rotate(self, rotation_degree):
+        init_coord = self.init_coordinates()
+        origin = init_coord[0]
+        rotated_coordinates_list = [origin]
+
+        if rotation_degree == 90:
+            print('Rotating 90 degrees...')
+            for i, coordinates in enumerate(init_coord):
+                if i == 0:
+                    continue
+
+                x = coordinates[0]
+                y = coordinates[1]
+                origin_x = origin[0]
+                origin_y = origin[1]
+                if y == origin_y:
+                    new_coord = [origin_x, origin_y + (x - origin_x)]
+                elif y - origin_y == self.sqsz:
+                    new_coord = [origin_x - (y - origin_y), origin_y + (x - origin_x)]
+                rotated_coordinates_list.append(new_coord)
+            print(rotated_coordinates_list)
+            return rotated_coordinates_list
+
+    @abstractmethod
+    def init_coordinates(self):
+        raise NotImplementedError
 
     @staticmethod
     def add_coordinates(first, second):
@@ -35,7 +64,7 @@ class Basic(Shape):
     def __init__(self, starting_coordinates, square_size):
         super().__init__(starting_coordinates, square_size)
 
-    def coordinates(self):
+    def init_coordinates(self):
         return [
             self.add_coordinates(self.sc, [0, 0]),
             self.add_coordinates(self.sc, [self.sqsz, 0]),
@@ -43,29 +72,24 @@ class Basic(Shape):
             self.add_coordinates(self.sc, [self.sqsz * 3, 0])
             ]
 
-    def rotations(self):
-        pass
-
 class SquareInMiddle(Shape):
     def __init__(self, starting_coordinates, square_size):
         super().__init__(starting_coordinates, square_size)
 
-    def coordinates(self):
+    def init_coordinates(self):
         return [
             self.add_coordinates(self.sc, [0, 0]),
             self.add_coordinates(self.sc, [self.sqsz, 0]),
             self.add_coordinates(self.sc, [self.sqsz * 2, 0]),
-            self.add_coordinates(self.sc, [self.sqsz * 3, 0])
+            self.add_coordinates(self.sc, [self.sqsz, self.sqsz])
         ]
 
-    def rotations(self):
-        pass
 
 class SquareOnLeft(Shape):
     def __init__(self, starting_coordinates, square_size):
         super().__init__(starting_coordinates, square_size)
 
-    def coordinates(self):
+    def init_coordinates(self):
         return [
             self.add_coordinates(self.sc, [0, 0]),
             self.add_coordinates(self.sc, [self.sqsz, 0]),
@@ -73,14 +97,11 @@ class SquareOnLeft(Shape):
             self.add_coordinates(self.sc, [0, self.sqsz])
         ]
 
-    def rotations(self):
-        pass
-
 class SquareOnRight(Shape):
     def __init__(self, starting_coordinates, square_size):
         super().__init__(starting_coordinates, square_size)
 
-    def coordinates(self):
+    def init_coordinates(self):
         return [
             self.add_coordinates(self.sc, [0, 0]),
             self.add_coordinates(self.sc, [self.sqsz, 0]),
@@ -88,14 +109,11 @@ class SquareOnRight(Shape):
             self.add_coordinates(self.sc, [self.sqsz * 2, self.sqsz])
         ]
 
-    def rotations(self):
-        pass
-
 class Zigzag(Shape):
     def __init__(self, starting_coordinates, square_size):
         super().__init__(starting_coordinates, square_size)
 
-    def coordinates(self):
+    def init_coordinates(self):
         return [
             self.add_coordinates(self.sc, [0, 0]),
             self.add_coordinates(self.sc, [self.sqsz, 0]),
@@ -103,5 +121,3 @@ class Zigzag(Shape):
             self.add_coordinates(self.sc, [self.sqsz * 2, self.sqsz])
         ]
 
-    def rotations(self):
-        pass
