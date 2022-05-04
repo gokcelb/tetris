97b1448990b6515f29fba9b294b1307ff8a7f452
gokcelbilgin <gokcelbilgin@gmail.com>
2022-05-04 14:02:11 +0300

    Turn rotate method into Shape's concrete method

--- shape/shape.py
+++ shape/shape.py
@@ -30,55 +30,41 @@ class Shape(ABC):
             self.rotate()
         return self.curr_coord_list
 
-    def calc_rotation(self, rotation_zone):
-        ref_coord = self.curr_coord_list[0]
-        ref_x = ref_coord[0]
-        ref_y = ref_coord[1]
-        rotated_coord_list = [ref_coord]
+    def rotate(self):
+        # const_x and const_y are the coordinates of the square
+        # that will always remain constant and unchanging
+        const_x = self.curr_coord_list[0][0]
+        const_y = self.curr_coord_list[0][1]
+        rotated_coord_list = [self.curr_coord_list[0]]
+        # ref_x and ref_y are coordinates that the method
+        # takes as reference to calculate rotation zones
+        ref_x = self.curr_coord_list[3][0]
+        ref_y = self.curr_coord_list[3][1]
+
+        zone_90 = ref_x > const_x and ref_y >= const_y
+        zone_180 = ref_x <= const_x and ref_y > const_y
+        zone_270 = ref_x < const_x and ref_y <= const_y
+        zone_360 = ref_x >= const_x and ref_y < const_y
 
         for i in range(1, len(self.curr_coord_list)):
             x = self.curr_coord_list[i][0]
             y = self.curr_coord_list[i][1]
 
-            if rotation_zone == 90:
-                new_coord = [ref_x - (y - ref_y), ref_y + (x - ref_x)]
-            elif rotation_zone == 180:
-                new_coord = [ref_x - (y - ref_y), ref_y - (ref_x - x)]
-            elif rotation_zone == 270:
-                new_coord = [ref_x + (ref_y - y), ref_y - (ref_x - x)]
-            elif rotation_zone == 360:
-                new_coord = [ref_x + (ref_y - y), ref_y + (x - ref_x)]
+            if zone_90:
+                new_coord = [const_x - (y - const_y), const_y + (x - const_x)]
+            elif zone_180:
+                new_coord = [const_x - (y - const_y), const_y - (const_x - x)]
+            elif zone_270:
+                new_coord = [const_x + (const_y - y), const_y - (const_x - x)]
+            elif zone_360:
+                new_coord = [const_x + (const_y - y), const_y + (x - const_x)]
             rotated_coord_list.append(new_coord)
 
         self.curr_coord_list = rotated_coord_list
-
-    def calc_zone(self):
-        ref_x = self.curr_coord_list[0][0]
-        ref_y = self.curr_coord_list[0][1]
-        x = self.curr_coord_list[3][0]
-        y = self.curr_coord_list[3][1]
-
-        zone_90 = x > ref_x and y >= ref_y
-        zone_180 = x <= ref_x and y > ref_y
-        zone_270 = x < ref_x and y <= ref_y
-        zone_360 = x >= ref_x and y < ref_y
-
-        if zone_90:
-            rotation_zone = 90
-        elif zone_180:
-            rotation_zone = 180
-        elif zone_270:
-            rotation_zone = 270
-        elif zone_360:
-            rotation_zone = 360
-        return rotation_zone
+        return self.curr_coord_list
 
     @abstractmethod
     def init_coordinates(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def rotate(self):
         raise NotImplementedError
 
     @staticmethod
@@ -97,10 +83,6 @@ class Basic(Shape):
             self.add_coordinates(self.sc, [self.sqsz * 3, 0])
             ]
 
-    def rotate(self):
-        self.calc_rotation(self.calc_zone())
-        return self.curr_coord_list
-
 class SquareInMiddle(Shape):
     def __init__(self, starting_coordinates, square_size):
         super().__init__(starting_coordinates, square_size)
@@ -112,10 +94,6 @@ class SquareInMiddle(Shape):
             self.add_coordinates(self.sc, [self.sqsz * 2, 0]),
             self.add_coordinates(self.sc, [self.sqsz, self.sqsz])
         ]
-
-    def rotate(self):
-        self.calc_rotation(self.calc_zone())
-        return self.curr_coord_list
 
 class SquareOnLeft(Shape):
     def __init__(self, starting_coordinates, square_size):
@@ -129,10 +107,6 @@ class SquareOnLeft(Shape):
             self.add_coordinates(self.sc, [0, self.sqsz])
         ]
 
-    def rotate(self):
-        self.calc_rotation(self.calc_zone())
-        return self.curr_coord_list
-
 class SquareOnRight(Shape):
     def __init__(self, starting_coordinates, square_size):
         super().__init__(starting_coordinates, square_size)
@@ -145,10 +119,6 @@ class SquareOnRight(Shape):
             self.add_coordinates(self.sc, [self.sqsz * 2, self.sqsz])
         ]
 
-    def rotate(self):
-        self.calc_rotation(self.calc_zone())
-        return self.curr_coord_list
-
 class Zigzag(Shape):
     def __init__(self, starting_coordinates, square_size):
         super().__init__(starting_coordinates, square_size)
@@ -160,7 +130,3 @@ class Zigzag(Shape):
             self.add_coordinates(self.sc, [self.sqsz, self.sqsz]),
             self.add_coordinates(self.sc, [self.sqsz * 2, self.sqsz])
         ]
-
-    def rotate(self):
-        self.calc_rotation(self.calc_zone())
-        return self.curr_coord_list
